bff59a41d49f02c2103a678e0099bfedf6b7cfa7
YusufKapukaya <62699767+YusufKapukaya@users.noreply.github.com>
2023-01-11 15:21:51 +0100

    Add files via upload

---
+++ src/app/create-user/create-user.component.ts
@@ -0,0 +1,53 @@
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, Validators, FormBuilder, AbstractControl } from '@angular/forms';
+
+
+@Component({
+  selector: 'app-create-user',
+  templateUrl: './create-user.component.html',
+  styleUrls: ['./create-user.component.css']
+})
+export class CreateUserComponent {
+
+  createUserForm!: FormGroup
+  // createUserForm= new FormGroup({
+  //   firstName: new FormControl(null, [Validators.required]),
+  //   lastName: new FormControl(null, [Validators.required]),
+  //   email: new FormControl(null, [Validators.required, Validators.email]),
+  //   password: new FormControl(null, [Validators.required,Validators.minLength(8)]),
+  //   repassword: new FormControl(null, [Validators.required,Validators.minLength(8)]),
+  //   adress: new FormControl(null, [Validators.required]),
+
+  //  })
+  constructor(private formBuilder: FormBuilder) { }
+  ngOnInit(): void {
+    this.createUserForm = this.formBuilder.group({
+      firstName: ["", [Validators.required]],
+      lastName: ["", [Validators.required]],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(8)]],
+      repassword: ["", [Validators.required, Validators.minLength(8)]],
+      adress: ["", [Validators.required]]
+    }, {
+      validators: this.matchinPasswords('passwords', 'repassword')
+    })
+  }
+
+
+  matchinPasswords(Password: string, ConfirmPassword: string) {
+    return (controls: AbstractControl) => {
+
+      if (controls) {
+        const Password = controls.get('password')!.value;
+        const ConfirmPassword = controls.get('repassword')!.value;
+        if(Password !==ConfirmPassword){
+          controls.get('repassword')?.setErrors({ not_the_same : true });
+          return {mismatchedPassword: true}
+        }
+      }
+      return null;
+    }
+
+  }
+}
+
